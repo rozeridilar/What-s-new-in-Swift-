78b7201d7ce14ad846f40ff512cc6ad00d696e61
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-04-08 13:55:55 +0300

    Pan Gesture Recognizer

--- Designs/Interactive 3D Carousel with Core Animation/Interactive 3D Carousel with Core Animation/ViewController.swift
+++ Designs/Interactive 3D Carousel with Core Animation/Interactive 3D Carousel with Core Animation/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
+func degreeToRadians(_ deg: CGFloat) -> CGFloat{
+    return (deg * CGFloat.pi) / 180
+}
+
 class ViewController: UIViewController {
 
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ViewController.performPanAction(recognizer:)))
+        
+        self.view.addGestureRecognizer(panGestureRecognizer)
     }
 
-
+    @objc func performPanAction(recognizer: UIPanGestureRecognizer){
+        
+    }
 }
 
