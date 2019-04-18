55d4a256dbe1d3afd6a48d9fd9c7003faa9f0382
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-04-18 09:39:41 +0300

    collection_view

---
+++ Designs/CollectinView/CollectinView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectinView
+//
+//  Created by Kızılay on 18.04.2019.
+//  Copyright © 2019 Rozeri Dilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
