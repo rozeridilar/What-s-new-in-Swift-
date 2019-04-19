ed779f730b565a4a53af6dcec69fa3b462676fa9
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-04-19 13:09:02 +0300

    create_image_array

--- Designs/UIImageViewAnimations/UIViewAnimations/ViewController.swift
+++ Designs/UIImageViewAnimations/UIViewAnimations/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heartImageView: UIImageView!
+    @IBOutlet weak var RyuImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func createImageArray(total: Int, image: String) -> [UIImage]{
+        let imageArray: [UIImage] = []
+        for index in 0...total{
+            imageArray.append(UIImage(named: "\(image)_\(index)"))
+        }
     }
 
 
