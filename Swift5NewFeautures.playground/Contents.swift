5a4da19a3c1579b6ba71c346caac0e39ed716798
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-05-09 15:10:14 +0300

    swift5

--- Swift5NewFeautures.playground/Contents.swift
+++ Swift5NewFeautures.playground/Contents.swift
@@ -1,3 +1,11 @@
-import Cocoa
+import Foundation
 
-var str = "Hello, playground"
+//new string capabilities
+let coolNewString = #"The keyword "var" is used to decleare a variable"#
+let multilineString = #"""
+Some new cool feautures
+are available in Swift 5 \#(coolNewString)
+"""#
+//Integer Multiples
+
+//Result Types
