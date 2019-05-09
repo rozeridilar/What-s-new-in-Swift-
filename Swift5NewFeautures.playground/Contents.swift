ace04d6d39d5f10a0c7e950dfa4969bf288f08a8
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-05-09 15:10:36 +0300

    swift5

--- Swift5NewFeautures.playground/Contents.swift
+++ Swift5NewFeautures.playground/Contents.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 //new string capabilities
-let coolNewString = #"The keyword "var" is used to decleare a variable"#
-let multilineString = #"""
-Some new cool feautures
-are available in Swift 5 \#(coolNewString)
-"""#
+//let coolNewString = #"The keyword "var" is used to decleare a variable"#
+//let multilineString = #"""
+//Some new cool feautures
+//are available in Swift 5 \#(coolNewString)
+//"""#
 //Integer Multiples
 
 //Result Types
