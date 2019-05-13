daa5cc1b0355eff293c30ed4630d2f9a92835279
Rozeri Dilar <rozeri.dilar@gmail.com>
2019-05-13 16:34:38 +0300

    coordinator

---
+++ Patterns/Coordinators/Coordinators/BuyViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BuyViewController.swift
+//  Coordinators
+//
+//  Created by Kızılay on 13.05.2019.
+//  Copyright © 2019 Rozeri Dilar. All rights reserved.
+//
+
+import UIKit
+
+class BuyViewController: UIViewController, Storyboarded {
+    weak var coordinator: MainCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Patterns/Coordinators/Coordinators/ViewController.swift
+++ Patterns/Coordinators/Coordinators/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController, Storyboarded {
 
+    weak var coordinator: MainCoordinator?
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buyTapped(_ sender: Any) {
+        coordinator?.buySubscription()
+    }
+    
+    @IBAction func createAccountTapped(_ sender: Any) {
+        coordinator?.createAccount()
+    }
 }
 
